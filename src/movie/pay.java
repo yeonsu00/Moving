e602c7b569bc89750ed31967ceb81f0321a854fb
kimbaekyu <qordb78@gmail.com>
2021-11-23 23:34:14 +0900

    Pay 이미지 파일 수정

--- src/movie/pay.java
+++ src/movie/pay.java
@@ -17,50 +17,62 @@ public class pay extends JFrame {
         JPanel top,mid,bot;
 
 
+
         top=new JPanel( new GridLayout(4,2));
         mid=new JPanel(new GridLayout(2,3));
         bot=new JPanel(new GridLayout(1,1));
         JRadioButton [] pay = new JRadioButton [4]; // 버튼 배열
-
-        //결제도구 이미지 처리 이미지파일 경로재수정해야됨
+        //패널 색 변경
+        top.setBackground(Color.WHITE);
+        mid.setBackground(Color.WHITE);
+        bot.setBackground(Color.WHITE);
+        //결제도구 이미지 처리
         //card 이미지 처리
-        ImageIcon cardImage = new ImageIcon("\\Users\\user\\IdeaProjects\\Moving\\src\\movie\\images\\cardButton.png");
-        //이미지를 실제로 갖고 있는 Image 객체 뽑아오기
+        ImageIcon cardImage = new ImageIcon("C:\\Users\\kim10\\IdeaProjects\\Moving\\src\\movie\\images\\cardButton.png");
+        //이미지를 실제로 갖고 있는 Image객체 뽑아오기
         Image card = cardImage.getImage();
         //뽑아온 이미지 객체 사이즈를 새롭게 만들기!
-        Image card2 = card.getScaledInstance(50, 50, Image.SCALE_DEFAULT);
+        Image card2 = card.getScaledInstance(400, 400, Image.SCALE_DEFAULT);
         //새로 조절된 사이즈의 이미지(im2)를 가지는 ImageIcon 객체를 다시 생성
         ImageIcon cardImage2 = new ImageIcon(card2);
 
         //phone 이미지 처리
-        ImageIcon phoneImage = new ImageIcon("\\Users\\user\\IdeaProjects\\Moving\\src\\movie\\images\\phone.png");
+        ImageIcon phoneImage = new ImageIcon("C:\\Users\\kim10\\IdeaProjects\\Moving\\src\\movie\\images\\phone.png");
         //이미지를 실제로 갖고 있는 Image객체 뽑아오기
         Image phone = phoneImage.getImage();
         //뽑아온 이미지 객체 사이즈를 새롭게 만들기!
-        Image phone2 = phone.getScaledInstance(30, 50, Image.SCALE_DEFAULT);
+        Image phone2 = phone.getScaledInstance(400, 400, Image.SCALE_DEFAULT);
         //새로 조절된 사이즈의 이미지(im2)를 가지는 ImageIcon 객체를 다시 생성
         ImageIcon phoneImage2 = new ImageIcon(phone2);
 
         //kakaopay 이미지 처리
-        ImageIcon kakaoImage = new ImageIcon("\\Users\\user\\IdeaProjects\\Moving\\src\\movie\\images\\kakao.png");
+        ImageIcon kakaoImage = new ImageIcon("C:\\Users\\kim10\\IdeaProjects\\Moving\\src\\movie\\images\\kakao.png");
         //이미지를 실제로 갖고 있는 Image객체 뽑아오기
         Image kakao = kakaoImage.getImage();
         //뽑아온 이미지 객체 사이즈를 새롭게 만들기!
-        Image kakao2 = kakao.getScaledInstance(50, 30, Image.SCALE_DEFAULT);
+        Image kakao2 = kakao.getScaledInstance(400, 400, Image.SCALE_DEFAULT);
         //새로 조절된 사이즈의 이미지(im2)를 가지는 ImageIcon 객체를 다시 생성
         ImageIcon kakaoImage2 = new ImageIcon(kakao2);
 
         //payco 이미지 처리
-        ImageIcon paycoImage = new ImageIcon("\\Users\\user\\IdeaProjects\\Moving\\src\\movie\\images\\payco.jpg");
+        ImageIcon paycoImage = new ImageIcon("C:\\Users\\kim10\\IdeaProjects\\Moving\\src\\movie\\images\\payco.png");
         //이미지를 실제로 갖고 있는 Image객체 뽑아오기
         Image payco = paycoImage.getImage();
         //뽑아온 이미지 객체 사이즈를 새롭게 만들기!
-        Image payco2 = payco.getScaledInstance(30, 20, Image.SCALE_DEFAULT);
+        Image payco2 = payco.getScaledInstance(400, 400, Image.SCALE_DEFAULT);
         //새로 조절된 사이즈의 이미지(im2)를 가지는 ImageIcon 객체를 다시 생성
         ImageIcon paycoImage2 = new ImageIcon(payco2);
 
+        //결제 이미지 처리
+        ImageIcon admitImage = new ImageIcon("C:\\Users\\kim10\\IdeaProjects\\Moving\\src\\movie\\images\\admitButton.png");
+        //이미지를 실제로 갖고 있는 Image객체 뽑아오기
+        Image admit = admitImage.getImage();
+        //뽑아온 이미지 객체 사이즈를 새롭게 만들기!
+        Image admit2 = admit.getScaledInstance(800, 32, Image.SCALE_DEFAULT);
+        //새로 조절된 사이즈의 이미지(im2)를 가지는 ImageIcon 객체를 다시 생성
+        ImageIcon admitImage2 = new ImageIcon(admit2);
         //선택버튼 추가
-        JButton bt = new JButton("선택");
+        JButton bt = new JButton(admitImage2);
 
         //top 패널 예매결과 출력
         num=new JLabel("총 인원");
@@ -82,10 +94,10 @@ public class pay extends JFrame {
         //mid 패널 결제종류선택
 
 
-        mid.add(pay[0]=new JRadioButton("신용/체크카드",cardImage2));
-        mid.add(pay[1]=new JRadioButton("휴대폰결제",phoneImage2));
-        mid.add(pay[2]=new JRadioButton("카카오페이",kakaoImage2));
-        mid.add(pay[3]=new JRadioButton("페이코",paycoImage2));
+        mid.add(pay[0]=new JRadioButton(cardImage2));
+        mid.add(pay[1]=new JRadioButton(phoneImage2));
+        mid.add(pay[2]=new JRadioButton(kakaoImage2));
+        mid.add(pay[3]=new JRadioButton(paycoImage2));
         // 라디오버튼 묶어줄 그룹 생성하고 add
         ButtonGroup jb = new ButtonGroup();
         jb.add(pay[0]);
@@ -103,7 +115,7 @@ public class pay extends JFrame {
         add(mid,BorderLayout.CENTER);
         add(bot,BorderLayout.SOUTH);
 
-        setSize(400,300);
+        setSize(800,1000);
         setVisible(true);
 
         //버튼 리스너
