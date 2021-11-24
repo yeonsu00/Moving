d64d9171f2a3172ecd522c387d77b3e2c91a9663
seeun01 <ssky601@gmail.com>
2021-11-24 16:36:01 +0900

    [김세은]

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ import movie.Login;
 public class Main {
     public static void main(String[] args) {
         System.out.println("HelloWorld");
+        System.out.println("HelloWorld");
+        System.out.println("HelloWorld");
         new Login();
     }
 }
