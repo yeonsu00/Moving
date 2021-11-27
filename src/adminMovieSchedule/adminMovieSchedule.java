dee919bf79616fc09efc94df2e445ac941d2f794
kangjjs <emdtod12@naver.com>
2021-11-27 12:00:15 +0900

    w

--- src/adminMovieSchedule/adminMovieSchedule.java
+++ src/adminMovieSchedule/adminMovieSchedule.java
@@ -1,86 +1,60 @@
-package adminMovie;
+package adminMovieSchedule;
 
-import javax.swing.*;
-import java.awt.BorderLayout;
-import java.awt.Container;
-import java.awt.Dimension;
+import java.util.Scanner;
 
-public class BorderLayoutDemo {
-    public static boolean RIGHT_TO_LEFT = false;
+public class adminMovieSchedule {
+    String code;
+    int StartHour;
+    int StartMinute;
+    int EndHour;
+    int EndMinute;
 
-    public static void addComponentsToPane(Container pane) {
-
-        if (!(pane.getLayout() instanceof BorderLayout)) {
-            pane.add(new JLabel("Container doesn't use BorderLayout!"));
-            return;
-        }
-
-        if (RIGHT_TO_LEFT) {
-            pane.setComponentOrientation(
-                    java.awt.ComponentOrientation.RIGHT_TO_LEFT);
-        }
-
-        JButton button = new JButton("Button 1 (PAGE_START)");
-        pane.add(button, BorderLayout.PAGE_START);
-
-        //Make the center component big, since that's the
-        //typical usage of BorderLayout.
-        button = new JButton("Button 2 (CENTER)");
-        button.setPreferredSize(new Dimension(200, 100));
-        pane.add(button, BorderLayout.CENTER);
-
-        button = new JButton("Button 3 (LINE_START)");
-        pane.add(button, BorderLayout.LINE_START);
-
-        button = new JButton("Long-Named Button 4 (PAGE_END)");
-        pane.add(button, BorderLayout.PAGE_END);
-
-        button = new JButton("5 (LINE_END)");
-        pane.add(button, BorderLayout.LINE_END);
+    adminMovieSchedule() {
     }
 
-    /**
-     * Create the GUI and show it.  For thread safety,
-     * this method should be invoked from the
-     * event dispatch thread.
-     */
-    private static void createAndShowGUI() {
-
-        //Create and set up the window.
-        JFrame frame = new JFrame("BorderLayoutDemo");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        //Set up the content pane.
-        addComponentsToPane(frame.getContentPane());
-        //Use the content pane's default BorderLayout. No need for
-        //setLayout(new BorderLayout());
-        //Display the window.
-        frame.pack();
-        frame.setVisible(true);
+    public adminMovieSchedule(Object[] row) {
+        code = (String) row[0];
+        StartHour = Integer.parseInt((String) row[1]);
+        StartMinute = Integer.parseInt((String) row[2]);
+        EndHour = Integer.parseInt((String) row[3]);
+        EndMinute = Integer.parseInt((String) row[4]);
     }
 
-    public static void main(String[] args) {
-        /* Use an appropriate Look and Feel */
-        try {
-            //UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
-            UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
-        } catch (UnsupportedLookAndFeelException ex) {
-            ex.printStackTrace();
-        } catch (IllegalAccessException ex) {
-            ex.printStackTrace();
-        } catch (InstantiationException ex) {
-            ex.printStackTrace();
-        } catch (ClassNotFoundException ex) {
-            ex.printStackTrace();
-        }
-        /* Turn off metal's use bold fonts */
-        UIManager.put("swing.boldMetal", Boolean.FALSE);
+    void read(Scanner scan) {
+        code = scan.next();
+        StartHour = scan.nextInt();
+        StartMinute = scan.nextInt();
+        EndHour = scan.nextInt();
+        EndMinute = scan.nextInt();
 
-        //Schedule a job for the event dispatch thread:
-        //creating and showing this application's GUI.
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                createAndShowGUI();
-            }
-        });
+
+    }
+
+    String[] getTexts() {
+        return new String[] { code,""+StartHour, ""+StartMinute, ""+EndHour, ""+EndMinute};
+    }
+
+    void print() {
+        System.out.printf("[%2s] %d %d %d %d \n", code,StartHour,StartMinute,EndHour,EndMinute );
+    }
+
+    boolean matches(String kwd) {
+        if (code.contains(kwd))
+            return true;
+        if (kwd.equals(""+StartHour))
+            return true;
+        if(kwd.equals(""+StartMinute))
+            return true;
+        if(kwd.equals(""+EndMinute))
+            return true;
+        if(kwd.equals(""+EndMinute))
+            return true;
+        return code.equals(kwd);
+
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s", code);
     }
 }
