0940e00eced49ef9f9ec17339731a3b585f336e5
kim-yeonsuu <yeonsu0329@kyonggi.ac.kr>
2021-12-02 01:45:39 +0900

    [김연수] 키오스크 수정 (광고 이미지 추가)

--- src/movie/Kiosk2.java
+++ src/movie/Kiosk2.java
@@ -13,43 +13,61 @@ import javax.swing.*;
 
 public class Kiosk2 extends JFrame {
     Kiosk2() {
-        setTitle("키오스크");
-        super.setPreferredSize(new Dimension(1000, 500));
-        setLayout(new GridLayout(3, 1));
-        JPanel namePanel = new JPanel();
-        namePanel.setLayout(new GridLayout(1, 2));
+        //setTitle("키오스크");
+        JFrame jf = new JFrame("키오스크");
+        jf.setSize(1950, 1040);
+        jf.setLocation(700, 400);
+        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JPanel panel2 = new JPanel();
-//        JButton button1 = new JButton("영화예매");
-//        JButton button2 = new JButton("매점");
-
         ImageIcon btnReservation = new ImageIcon("image/btnReservation.png");
         Image image1 = btnReservation.getImage();
         Image changeImg1 = image1.getScaledInstance(360, 200, Image.SCALE_SMOOTH);
         ImageIcon changeIcon1 = new ImageIcon(changeImg1);
-
         JLabel la1 = new JLabel(changeIcon1);
 
         ImageIcon btnFood = new ImageIcon("image/btnFood.png");
         Image image2 = btnFood.getImage();
         Image changeImg2 = image2.getScaledInstance(360, 200, Image.SCALE_SMOOTH);
         ImageIcon changeIcon2 = new ImageIcon(changeImg2);
-
         JLabel la2 = new JLabel(changeIcon2);
-
-        //
-        JPanel loginPanel = new JPanel();
-        JPanel noPanel = new JPanel();
-        JButton loginBtn = new JButton("로그인 및 회원가입");
 
         panel2.add(la1);
         panel2.add(la2);
-        loginPanel.add(loginBtn);
-        add(panel2);
-        add(noPanel);
-        add(loginPanel);
+        jf.add("North", panel2);
+        JPanel jp = new JPanel();
+        jp.setLayout(new BoxLayout(jp, BoxLayout.Y_AXIS));
 
-        super.pack();
+        JPanel loginPanel = new JPanel();
+        //JPanel noPanel = new JPanel();
+        JButton loginBtn = new JButton("로그인 및 회원가입");
+
+        JPanel imgPanel = new JPanel();
+        ImageIcon img = new ImageIcon("image/KioskImg.png");
+        Image image3 = img.getImage();
+        Image changeImg3 = image3.getScaledInstance(1200, 500, Image.SCALE_SMOOTH);
+        ImageIcon changeIcon3 = new ImageIcon(changeImg3);
+
+        JLabel la3 = new JLabel(changeIcon3);
+        imgPanel.add(la3);
+
+        JPanel jp1 = new JPanel();
+        jp1.setLayout(new BorderLayout());
+        JPanel jp2 = new JPanel();
+        jp2.setLayout(new BorderLayout());
+        JPanel jp3 = new JPanel();
+        jp3.setLayout(new BorderLayout());
+
+        loginPanel.add(loginBtn);
+        jp1.add("Center", loginPanel);
+        jp2.add("Center", imgPanel);
+
+        jp3.add("North", jp1);
+        jp3.add("Center", jp2);
+        jp.add(jp3);
+        jf.add("Center", jp);
+       // jf.getContentPane().setBackground(Color.WHITE);
+
 
         la1.addMouseListener(new MouseAdapter() {
             @Override
@@ -74,8 +92,7 @@ public class Kiosk2 extends JFrame {
                 dispose();
             }
         });
-
-        setVisible(true);
+        jf.setVisible(true);
     }
 
     Kiosk2(int i) {
@@ -103,9 +120,18 @@ public class Kiosk2 extends JFrame {
 
         JLabel la2 = new JLabel(changeIcon2);
 
+//        ImageIcon img = new ImageIcon("image/KioskImg.png");
+//        Image image3 = img.getImage();
+//        Image changeImg3 = image3.getScaledInstance(1000, 800, Image.SCALE_SMOOTH);
+//        ImageIcon changeIcon3 = new ImageIcon(changeImg3);
+//
+//        JLabel la3 = new JLabel(changeIcon3);
+
         panel2.add(la1);
         panel2.add(la2);
+        //panel3.add(la3);
         add(panel2);
+        //add(panel3);
 
         super.pack();
 
